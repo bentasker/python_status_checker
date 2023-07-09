d366dd62641f854619e387909d3d39039b8fcd1a
B Tasker <github@bentasker.co.uk>
2023-07-09 11:12:19 +0100

    Add HTTP/2 checks

--- status_check.py
+++ status_check.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 #
+# pip install requests httpx[http2]
 #
-#
-
+import httpx
 import requests
 import time
 
@@ -11,26 +11,29 @@ from requests.adapters import HTTPAdapter
 
 urls = [
    { "url": "https://mastodon.bentasker.co.uk/", "check" : ["h1"] },
-   { "url": "https://www.bentasker.co.uk/", "check" : ["h1"] }
+   { "url": "https://www.bentasker.co.uk/", "check" : ["h1", "h2"] }
 ]
 
-
-
-def do_check(url):
-    ''' Send a probe to the URL and record specifics about it
-    
-    '''
-    url_result = {
+def get_check_dict(url):
+    return {
         "url" : url,
-        "http_version" : "1.1"
+        "http_version" : "1.1",
         "status" : "0", # 0 - down, 1 - up
         "failure_reason" :  "nofail",
         "request_duration_ns" : 0,
         "status_code" : 0,
         "bytes_transferred" : 0
-        }
+    } 
 
-    headers = {}
+def get_request_headers():
+    return {}
+
+def do_h1_check(url):
+    ''' Send a HTTP/1.1 probe to the URL and record specifics about it
+    '''
+
+    url_result = get_check_dict(url)
+    headers = get_request_headers()
     failed = False
     try:
         # We don't want any retries - it should fail first time 
@@ -46,10 +49,17 @@ def do_check(url):
 
     except Exception as e:
         stop = time.time_ns()
-        print(f"aaaaa {e}")
         url_result["failure_reason"] = match_exception_string(str(e).lower())
         failed = True
     
+    return process_result(res, url_result, failed, start, stop)
+
+    
+def process_result(res, url_result, failed, start, stop):
+    ''' Populate the result dict 
+    
+        res should be a requests compatible object
+    '''
     # Calculate the timings
     url_result['request_duration_ns'] = stop - start
     
@@ -73,16 +83,50 @@ def do_check(url):
         # for consistency between responses
         url_result['bytes_transferred'] = len(res.text)
     
+    url_result['status'] = 1
     return url_result
     
+    
+def do_h2_check(url):
+    url_result = get_check_dict(url)
+    headers = get_request_headers()
+    failed = False
+    url_result["http_version"] = "2"
+    
+    try:
+        start = time.time_ns()
+        client = httpx.Client(http2=True)
+        res = client.get(url)
+        stop = time.time_ns()
+    except Exception as e:
+        stop = time.time_ns()
+        url_result["failure_reason"] = match_exception_string(str(e).lower())
+        failed = True
+        # If we failed to connect, res won't exist
+        # prevent exceptions
+        res = False
+        
+    if not failed and res.http_version != "HTTP/2":
+        # Although we might have got a response it wasn't H2
+        # so it's a fail
+        url_result['status'] = 0
+        url_result["failure_reason"] = "not-http2"
+        failed = True
+        
+    return process_result(res, url_result, failed, start, stop)    
+        
     
 def main(check_urls):
     # iterate through the urls
     for url in check_urls:
         if "h1" in url['check']:
-            http1_result = do_check(url['url'])
+            http1_result = do_h1_check(url['url'])
             print(http1_result)
 
+        if "h2" in url['check']:
+            http2_result = do_h2_check(url['url'])
+            print(http2_result)
+            
 
 def match_exception_string(s):
     ''' Take an exception string and check for certain 
@@ -105,6 +149,6 @@ def match_exception_string(s):
     
     
 
-
-main(urls)
+if __name__ == "__main__":
+    main(urls)
 
